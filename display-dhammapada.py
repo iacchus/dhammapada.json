bf74c098f0ae3bdf15dc058f311ced6f0f162ea7
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-02-19 15:22:23 -0300

    commit

--- display-dhammapada.py
+++ display-dhammapada.py
@@ -1,27 +1,15 @@
-## the Dhammapada
-### in `.json` format
-
-two translations
-
-generated with `./dhammapada-json.py`
-
-### how to use
-
-1. sort a number between 0 and 346
-2. get a verse
-
-### example script
-
-```
 #!/usr/bin/env python
 
 import json
 import random
 
-with open("./dhammapada.json", "r") as dhammapada_json_file:
+DHAMMAPADA_JSON_FILEPATH = "./dhammapada.json"
+
+with open(DHAMMAPADA_JSON_FILEPATH, "r") as dhammapada_json_file:
     dhammapada_json = json.load(dhammapada_json_file)
 
 keys = dhammapada_json.keys()
+
 random_choice = random.choice(list(keys))
 
 verse_numbers, verse = dhammapada_json[random_choice]
@@ -31,10 +19,9 @@ signature = f"— The Dhammapada, {verses}"
 
 print(verse, signature, sep="\n\n")
 
-#  $ python display-dhammapada.py
+#  $ ./display-dhammapada.py
 #  Cut away the five (lower fetters), abandon the five (remaining fetters),
 #  and then develop the five (faculties). The bhikkhu who has transcended
 #  the five fetters is said to be "crossed over the flood".
 #
 #  — The Dhammapada, 370
-```
